6d900a8ada2755b8e46cfcc7cf68cba64bb81060
microlao <63964055+microlao@users.noreply.github.com>
2020-06-05 18:10:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,18 +8,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-ssr-plus
-pushd package/lean
-git clone --depth=1 https://github.com/fw876/helloworld
-popd
-
 # Clone community packages to package/community
-mkdir package/community
-pushd package/community
-
-# Add Lienol's Packages
-git clone --depth=1 https://github.com/SuLingGG/openwrt-package
-
+mkdir package/diy
+pushd package/diy
 # Add ServerChan
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 
@@ -27,13 +18,8 @@ git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
-
-# Add udptools
-git clone --depth=1 https://github.com/bao3/openwrt-udp2raw
-git clone --depth=1 https://github.com/bao3/openwrt-udpspeeder
-git clone --depth=1 https://github.com/bao3/luci-udptools
+svn co https://github.com/pymumu/smartdns/trunk/package/openwrt smartdns
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns luci-app-smartdn
 
 # Add OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
