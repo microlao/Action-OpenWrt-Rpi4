64d0ebfee92759026df159f00d067620e1606481
microlao <63964055+microlao@users.noreply.github.com>
2020-07-15 15:22:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Clone community packages to package/community
 mkdir package/diy
@@ -21,6 +21,4 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 svn co https://github.com/pymumu/smartdns/trunk/package/openwrt smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns luci-app-smartdn
 
-# Add OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
 popd
