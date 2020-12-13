9af90a39456a496cd5be6ee3cf457dbf0f8005a3
microlao <63964055+microlao@users.noreply.github.com>
2020-12-13 22:59:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Clone community packages to package/community
 mkdir package/diy
